cb10fd86072fd5ae1aeadda41ad2cb5ca2543ea1
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-15 19:29:28 +0530

    1

---
+++ main.py
@@ -0,0 +1,22 @@
+#Radhe Radhe 
+#Data series in pandas
+
+import pandas as pd
+
+data=[1,2,3,4,5,6,7,8,9,100]
+series=pd.Series(data)
+
+print(series)
+
+#OUTPUT
+# 0      1
+# 1      2
+# 2      3
+# 3      4
+# 4      5
+# 5      6
+# 6      7
+# 7      8
+# 8      9
+# 9    100
+# dtype: int64
