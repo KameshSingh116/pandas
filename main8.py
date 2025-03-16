e16604123f198fb48c919dbb2f86c6df6cff2791
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-16 18:57:19 +0530

    update

--- main8.py
+++ main8.py
@@ -4,3 +4,21 @@
 
 import pandas as pd
 import numpy as np
+
+data={
+    "Name":['Deviji','Kamesh','Ritish','Shivangi'],
+    "Age":[19,29,np.nan,19],
+    "City":['Mera Dill','Kanhaji ke charan','jammu','jammu']
+}
+
+frame=pd.DataFrame(data)
+
+print(frame)
+
+#output
+#        Name   Age               City
+# 0    Deviji  19.0          Mera Dill
+# 1    Kamesh  29.0  Kanhaji ke charan
+# 2    Ritish   NaN              jammu
+# 3  Shivangi  19.0              jammu
+
