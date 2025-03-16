cb1fb76255ab3d6c50e177a081d8b20086e3ec96
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-16 15:31:39 +0530

    main3.2.py

--- main3.py
+++ main3.py
@@ -15,3 +15,36 @@ frame=pd.DataFrame(data)
 
 print(frame.loc[0])
 print(frame.loc[1])
+
+#OUTPUT
+# Name       Deviji
+# Age            19
+# City    Mera Dill
+# Name: 0, dtype: object
+# Name               Kamesh
+# Age                    29
+# City    Kanhaji ke charan
+# Name: 1, dtype: object
+
+#For multiple rows
+print(frame.loc[0:1])
+
+#      Name  Age               City
+# 0  Deviji   19          Mera Dill
+# 1  Kamesh   29  Kanhaji ke charan
+
+
+#2)We use iloc[] when selecting rows based on index.
+
+print(frame.iloc[0])
+
+
+#OUTPUT
+# Name       Deviji
+# Age            19
+# City    Mera Dill
+# Name: 0, dtype: object
+
+#For loc[] , we use labels as a reference to select rows.
+#For iloc[], we use index as a reference to select rows.
+#Radhe Radhe
