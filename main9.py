28571e3d2db46d57ee735ecde6d386376d67e36d
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-16 19:34:54 +0530

    main9.py

--- main9.py
+++ main9.py
@@ -5,3 +5,25 @@
 
 import pandas as pd
 import numpy as np
+
+data={
+    "Name":['Deviji','Kamesh','Ritish','Shivangi'],
+    "Age":[19,np.nan,np.nan,19],
+    "City":['Mera Dill','Kanhaji ke charan','jammu','jammu']
+}
+frame=pd.DataFrame(data)
+print(frame)
+
+#output
+#        Name   Age               City
+# 0    Deviji  19.0          Mera Dill
+# 1    Kamesh   NaN  Kanhaji ke charan
+# 2    Ritish   NaN              jammu
+# 3  Shivangi  19.0              jammu
+
+# we will use dropna() method to remove rows with missing values
+# frame.dropna(inplace=True)
+# #inplace=True will modify the original dataframe
+# print(frame)
+
+print(frame.dropna(inplace=True)) #None
