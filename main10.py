bfe267a929b21575e2b05e11b55a58439aa58246
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-16 20:42:44 +0530

    importing required modules

---
+++ main10.py
@@ -0,0 +1,6 @@
+#Radhe Radhe
+# Filling Missing Values in Pandas (fillna())
+# Instead of dropping missing values, you can replace them with meaningful values using .fillna().
+
+import pandas as pd
+import numpy as np
